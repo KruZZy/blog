91a28b94ff3dd3343219e305e266aa0488a33369
KruZZy <visoiuandrei42@yahoo.ro>
2022-04-08 20:27:16 +0200

    Add API routes for updating and deleting posts.

--- django_src/api/post/urls.py
+++ django_src/api/post/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.PostAPI.as_view()),
+    path('<int:pk>', views.PostRetrieveUpdateDestroyAPI.as_view())
 ]
--- django_src/api/post/views.py
+++ django_src/api/post/views.py
@@ -1,5 +1,6 @@
+from django.db.models import QuerySet
 from rest_framework import status
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
@@ -8,22 +9,56 @@ from . import serializer
 
 
 class PostAPI(ListCreateAPIView):
-    query_set = post.objects.all()
     serializer_class = serializer.postSerializer
     permission_classes = (AllowAny,)
 
-    def get_queryset(self):
-        return self.query_set
+    def get_queryset(self) -> QuerySet:
+        """
+        This view should return the list of all the posts in the DB.
+        :return: a QuerySet containing all the posts in the DB.
+        """
+        return post.objects.all()
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response({"status": True,
-                        "message": "Post added.",
-                        "data": serializer.data},
-            status=status.HTTP_201_CREATED, headers=headers);
+    def create(self, request, *args, **kwargs) -> Response:
+        obj_serializer = self.get_serializer(data=request.data)
+        obj_serializer.is_valid(raise_exception=True)
+        self.perform_create(obj_serializer)
+        headers = self.get_success_headers(obj_serializer.data)
+        return Response(
+            {
+                "status": True,
+                "message": "Post added.",
+                "data":  obj_serializer.data
+            },
+            status=status.HTTP_201_CREATED, headers=headers
+        )
 
 
+class PostRetrieveUpdateDestroyAPI(RetrieveUpdateDestroyAPIView):
+    serializer_class = serializer.postSerializer
 
+    def get_queryset(self) -> QuerySet:
+        """
+        Returns a post by its ID.
+        :return: a post object by an ID, if it exists.
+        An HTTP response with status 404 if it doesn't.
+        """
+        return post.objects.filter(id=self.kwargs.get('pk', None))
+
+    def update(self, request, *args, **kwargs) -> Response:
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        obj_serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        obj_serializer.is_valid(raise_exception=True)
+        self.perform_update(obj_serializer)
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(
+            {
+                "status": True,
+                "message": "Post update successfully.",
+                "data": obj_serializer.data
+            }
+        )
